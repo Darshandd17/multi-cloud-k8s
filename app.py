daa2600b0bc3a470feba58a2bfc25e157fca22d1
Darshandd17 <darshandd592@gmail.com>
2025-02-20 20:21:48 +0530

    Updated GitHub Actions for ECR deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def hello():
     return "Hello from Multi-Cloud Kubernetes Deployment!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv("PORT", 5000))  # Get port from env variable (default 5000)
+    app.run(host='0.0.0.0', port=port)
+
