173054fa0c9f7a027e95b02fadacc06fa68e15f7
Darshandd17 <darshandd592@gmail.com>
2025-02-20 20:38:37 +0530

    Updated deployment.yaml

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
@@ -8,6 +7,4 @@ def hello():
     return "Hello from Multi-Cloud Kubernetes Deployment!"
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))  # Get port from env variable (default 5000)
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0', port=5000)
