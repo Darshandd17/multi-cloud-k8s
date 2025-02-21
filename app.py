b39b6f9a79973f1f8777f86a0fc60332c44450b2
Darshandd17 <darshandd592@gmail.com>
2025-02-21 13:47:11 +0530

    Updated-features

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def hello():
     return "Hello from Multi-Cloud Kubernetes Deployment!"
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Oops! Page not found.", 404
+
+@app.route('/portal/redlion')
+def redlion():
+    return "Welcome to Redlion Portal!"
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not set
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, threaded=True)
